049d7df14bdc40bbbaeb3f106452bf0f6bcc31cc
Mariel Torres <marieltorres97@gmail.com>
2022-09-15 23:06:11 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavBarComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule
