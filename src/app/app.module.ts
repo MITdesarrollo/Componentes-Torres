e05d469b56722be8006e9428ff599d5f46addb1e
Mariel Torres <marieltorres97@gmail.com>
2022-09-18 00:04:11 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { StudentsComponent } from './students/students.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { NavBarComponent } from './componentes/nav-bar/nav-bar.component';
+import { StudentsComponent } from './componentes/students/students.component';
 
 @NgModule({
   declarations: [
